1bbf43439d5d14350338acdebde998f986649ca7
Jonathan Moallem <jdmoallem@gmail.com>
2019-12-15 11:24:51 -0500

    Made player movement work properly with tank-type controls

--- player.gd
+++ player.gd
@@ -5,10 +5,23 @@ var direction = 0
 
 func _input(event):
 	if Input.is_action_pressed('up'):
-		location.y -= 1
+		move(+1)
 	if Input.is_action_pressed('down'):
-		location.y += 1
+		move(-1)
 	if Input.is_action_pressed('left'):
-		direction = (direction - 1) % 4
+		direction = (direction - 1) % 4	
 	if Input.is_action_pressed('right'):
 		direction = (direction + 1) % 4
+	if direction < 0:
+		direction = 4 + direction
+
+func move(amount):
+	match direction:
+		0:
+			location.y -= amount
+		1:
+			location.x += amount
+		2:
+			location.y += amount
+		3:
+			location.x -= amount
