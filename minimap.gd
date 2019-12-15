9ba224f2b2f65ed27c04df15122337d79ae9401c
Jonathan Moallem <jdmoallem@gmail.com>
2019-12-15 10:14:48 -0500

    Made player script get player input to move player

--- minimap.gd
+++ minimap.gd
@@ -3,7 +3,7 @@ extends Node2D
 var player_loc = Vector2(0, 0)
 var player_direction = 0
 
-var direction_chars = ["▲", "▼", "◄", "►"]
+var direction_chars = ["▲", "►", "▼", "◄"]
 
 var wall_map  = [
 ["■", "■", "■", "■"],
@@ -21,18 +21,26 @@ var entity_map  = [
 [" ", " ", " ", " "],
 ]
 
-func _ready():
+func _physics_process(delta):
 	player_loc = get_parent().location
 	player_direction = get_parent().direction
-
-func _physics_process(delta):
+	
+	var temp_entity_map = []
+	
+	var i = 0
+	for val in entity_map:
+		temp_entity_map.append([])
+		for v in val:
+			temp_entity_map[i].append(v)
+		i += 1
+	
 	var wall_rows = []
 	var entity_rows = []
 	for i in range(wall_map.size()):
 		if i == player_loc.y:
-			entity_map[i][player_loc.x] = direction_chars[player_direction]
+			temp_entity_map[i][player_loc.x] = direction_chars[player_direction]
 		wall_rows.append(PoolStringArray(wall_map[i]).join(""))
-		entity_rows.append(PoolStringArray(entity_map[i]).join(""))
+		entity_rows.append(PoolStringArray(temp_entity_map[i]).join(""))
 	var flattened_wall_map = PoolStringArray(wall_rows).join("\n")
 	var flattened_entity_map = PoolStringArray(entity_rows).join("\n")
 	
--- player.gd
+++ player.gd
@@ -3,5 +3,12 @@ extends Node2D
 var location = Vector2(1, 3)
 var direction = 0
 
-func move():
-	pass
+func _input(event):
+	if Input.is_action_pressed('up'):
+		location.y -= 1
+	if Input.is_action_pressed('down'):
+		location.y += 1
+	if Input.is_action_pressed('left'):
+		direction = (direction - 1) % 4
+	if Input.is_action_pressed('right'):
+		direction = (direction + 1) % 4
