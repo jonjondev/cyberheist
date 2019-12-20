7f1dd32bae69ab586aba69c09d977af088d27541
Jonathan Moallem <jdmoallem@gmail.com>
2019-12-20 07:49:28 -0500

    Updated minimap to use bbcode text for styling elements

--- minimap.gd
+++ minimap.gd
@@ -11,7 +11,10 @@ func _physics_process(delta):
 	for i in range(temp_map.size()):
 		if i == grid.player_loc.y:
 			temp_map[i][grid.player_loc.x] = direction_chars[grid.player_direction]
+		for j in range(temp_map[i].size()):
+			if direction_chars.has(temp_map[i][j]):
+				temp_map[i][j] = "[color=fuchsia]" + temp_map[i][j] + "[/color]"
 		rows.append(PoolStringArray(temp_map[i]).join(""))
 	var flattened_map = PoolStringArray(rows).join("\n")
 	
-	text = flattened_map
+	bbcode_text = flattened_map
