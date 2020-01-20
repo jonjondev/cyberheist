626b870df3122f26b39321cf80fa76c3638c93a4
Jonathan Moallem <jdmoallem@gmail.com>
2020-01-20 16:36:44 +1100

    Made visualisation hide walls that are too far away

--- visualisation.gd
+++ visualisation.gd
@@ -13,8 +13,13 @@ func _physics_process(delta):
 	
 	if player_direction == 0:
 		if map[player_location.y-1][player_location.x] == "■":
+			$Wall.visible = true
 			$Wall.texture = walls[0]
 		elif map[player_location.y-2][player_location.x] == "■":
+			$Wall.visible = true
 			$Wall.texture = walls[1]
 		elif map[player_location.y-3][player_location.x] == "■":
+			$Wall.visible = true
 			$Wall.texture = walls[2]
+		else:
+			$Wall.visible = false
