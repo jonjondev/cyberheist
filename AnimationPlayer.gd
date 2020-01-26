2048e46d1ba5df8f062b332cad7a8345cffcd576
Jonathan Moallem <jdmoallem@gmail.com>
2020-01-26 13:41:59 +1100

    Changed context switching key to tab

--- AnimationPlayer.gd
+++ AnimationPlayer.gd
@@ -3,7 +3,7 @@ extends AnimationPlayer
 var minimised = false
 
 func _input(event):
-	if Input.is_action_pressed('escape'):
+	if Input.is_action_pressed('tab'):
 		if minimised:
 			play("maximise")
 		else:
