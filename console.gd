42970bfe3d741068d0c49bc99ccd62d4bcce6c39
Jonathan Moallem <jdmoallem@gmail.com>
2020-01-26 14:10:54 +1100

    Added the ability to type lower case character to console

--- console.gd
+++ console.gd
@@ -1,4 +1,15 @@
 extends Label
 
-func _ready():
-	text = text + " ▋"
+#func _ready():
+	#text = text + " ▋"
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		var event_key = event.as_text()
+		var new_char = ""
+		if event_key.length() == 1:
+			new_char = event.as_text().to_lower()
+		else:
+			#new_char = event_key
+			pass
+		text = text + new_char
