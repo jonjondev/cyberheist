677c3853109f8c22306acdfa7aa14b5270a72875
Jonathan Moallem <jdmoallem@gmail.com>
2020-01-26 14:17:07 +1100

    Stopped console from receiving input while closed

--- console.gd
+++ console.gd
@@ -3,25 +3,28 @@ extends Label
 #func _ready():
 	#text = text + " ▋"
 
+var awake = false
+
 func _input(event):
-	if event is InputEventKey and event.pressed:
-		var event_key = event.as_text()
-		var new_char = ""
-		if event_key.length() == 1:
-			new_char = event.as_text().to_lower()
-		elif event_key == "Space":
-			new_char = " "
-		elif event_key == "Period":
-			new_char = "."
-		elif event_key == "Enter":
-			new_char = "\n> "
-		elif event_key == "BackSpace":
-			if text.substr(text.length() - 2, text.length()) != "> ":
-				text = text.left(text.length() - 1)
-		elif event_key.begins_with("Shift+"):
-			# Add modifiers here
-			new_char = "!!!"
-		else:
-			#new_char = event_key
-			pass
-		text = text + new_char
+	if awake:
+		if event is InputEventKey and event.pressed:
+			var event_key = event.as_text()
+			var new_char = ""
+			if event_key.length() == 1:
+				new_char = event.as_text().to_lower()
+			elif event_key == "Space":
+				new_char = " "
+			elif event_key == "Period":
+				new_char = "."
+			elif event_key == "Enter":
+				new_char = "\n> "
+			elif event_key == "BackSpace":
+				if text.substr(text.length() - 2, text.length()) != "> ":
+					text = text.left(text.length() - 1)
+			elif event_key.begins_with("Shift+"):
+				# Add modifiers here
+				new_char = "!!!"
+			else:
+				#new_char = event_key
+				pass
+			text = text + new_char
