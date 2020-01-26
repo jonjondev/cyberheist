a1031c282a5b3920d83387f80a9708ad2abaea96
Jonathan Moallem <jdmoallem@gmail.com>
2020-01-26 14:12:35 +1100

    Added more special characters to console typing

--- console.gd
+++ console.gd
@@ -9,6 +9,13 @@ func _input(event):
 		var new_char = ""
 		if event_key.length() == 1:
 			new_char = event.as_text().to_lower()
+		elif event_key == "Space":
+			new_char = " "
+		elif event_key == "Period":
+			new_char = "."
+		elif event_key.begins_with("Shift+"):
+			# Add modifiers here
+			new_char = "!!!"
 		else:
 			#new_char = event_key
 			pass
