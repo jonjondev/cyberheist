e2d80ff0624a9ea427ddfce3739715c2f81b52a7
Jonathan Moallem <jdmoallem@gmail.com>
2020-01-29 13:02:11 +1100

    Merged input maps

--- console.gd
+++ console.gd
@@ -2,28 +2,40 @@ extends Label
 
 var regex_alphabetical
 
-var shift_input_map = {
-	'1': '!',
-	'2': '@',
-	'3': '#',
-	'4': '$',
-	'5': '%',
-	'6': '^',
-	'7': '&',
-	'8': '*',
-	'9': '(',
-	'0': ')',
-	'QuoteLeft': '~',
-	'Minus': '_',
-	'Equal': '+',
-	'BraceLeft': '{',
-	'BraceRight': '}',
-	'BackSlash': '|',
-	'Semicolon': ':',
-	'Apostrophe': '"',
-	'Comma': '<',
-	'Period': '>',
-	'Slash': '?',
+var character_input_map = {
+	'QuoteLeft': '`',
+	'Minus': '-',
+	'Equal': '=',
+	'BraceLeft': '[',
+	'BraceRight': ']',
+	'BackSlash': '\\',
+	'Semicolon': ';',
+	'Apostrophe': '\'',
+	'Comma': ',',
+	'Period': '.',
+	'Slash': '/',
+	'Space': ' ',
+	'Shift+1': '!',
+	'Shift+2': '@',
+	'Shift+3': '#',
+	'Shift+4': '$',
+	'Shift+5': '%',
+	'Shift+6': '^',
+	'Shift+7': '&',
+	'Shift+8': '*',
+	'Shift+9': '(',
+	'Shift+0': ')',
+	'Shift+QuoteLeft': '~',
+	'Shift+Minus': '_',
+	'Shift+Equal': '+',
+	'Shift+BraceLeft': '{',
+	'Shift+BraceRight': '}',
+	'Shift+BackSlash': '|',
+	'Shift+Semicolon': ':',
+	'Shift+Apostrophe': '"',
+	'Shift+Comma': '<',
+	'Shift+Period': '>',
+	'Shift+Slash': '?',
 }
 
 func _ready():
@@ -38,25 +50,19 @@ func _input(event):
 		if event is InputEventKey and event.pressed:
 			var event_key = event.as_text()
 			var new_char = ""
-			if event_key.length() == 1:
+			var special_character = character_input_map.get(event_key)
+			if special_character:
+				new_char = special_character
+			elif event_key.length() == 1:
 				new_char = event.as_text().to_lower()
-			elif event_key == "Space":
-				new_char = " "
-			elif event_key == "Period":
-				new_char = "."
 			elif event_key == "Enter":
 				new_char = "\n> "
 			elif event_key == "BackSpace":
 				if text.substr(text.length() - 2, text.length()) != "> ":
 					text = text.left(text.length() - 1)
 			elif event_key.begins_with("Shift+"):
-				event_key = event_key.replace("Shift+", "")
-				if event_key.length() == 1 and regex_alphabetical.search(event_key):
-					new_char = event_key
-				else:
-					new_char = shift_input_map.get(event_key)
-			else:
-				#new_char = event_key
-				pass
+				var stripped_event_key = event_key.replace("Shift+", "")
+				if stripped_event_key.length() == 1 and regex_alphabetical.search(stripped_event_key):
+					new_char = stripped_event_key
 			if new_char:
 				text = text + new_char
