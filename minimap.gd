dec1c3791270d7ed53585b3ac43a8c372b3d14ed
Jonathan Moallem <jdmoallem@gmail.com>
2020-02-03 16:14:09 +1100

    Added entities back into minimap display

--- minimap.gd
+++ minimap.gd
@@ -6,21 +6,25 @@ var direction_chars = ["▲", "►", "▼", "◄"]
 
 func _physics_process(delta):
 	if grid.wall_map:
-		var temp_map = grid.wall_map.duplicate(true)
+		var merge_map = grid.wall_map.duplicate(true)
 		
-		temp_map = [[" ", " ", " ", " ", " "],
-					[" ", " ", " ", " ", " "],
-					[" ", " ", " ", " ", " "],
-					[" ", " ", " ", " ", " "],
-					[" ", " ", " ", " ", " "],]
+		for location in grid.entities.keys():
+			merge_map[location.y][location.x] = grid.entities[location]
+		
+		var temp_map = [[" ", " ", " ", " ", " "],
+						[" ", " ", " ", " ", " "],
+						[" ", " ", " ", " ", " "],
+						[" ", " ", " ", " ", " "],
+						[" ", " ", " ", " ", " "],]
+		
 		var y_offset = -2
 		for i in range(temp_map.size()):
 			var x_offset = -2
 			for j in range(temp_map[i].size()):
 				var y_index = grid.player_loc.y+y_offset
 				var x_index = grid.player_loc.x+x_offset
-				if  y_index < grid.wall_map.size() and y_index >= 0 and x_index < grid.wall_map[y_index].size() and x_index >= 0:
-					temp_map[i][j] = grid.wall_map[y_index][x_index]
+				if  y_index < merge_map.size() and y_index >= 0 and x_index < merge_map[y_index].size() and x_index >= 0:
+					temp_map[i][j] = merge_map[y_index][x_index]
 				else:
 					temp_map[i][j] = "■"
 				x_offset += 1
@@ -33,6 +37,8 @@ func _physics_process(delta):
 			for j in range(temp_map[i].size()):
 				if direction_chars.has(temp_map[i][j]):
 					temp_map[i][j] = "[color=fuchsia]" + temp_map[i][j] + "[/color]"
+				elif temp_map[i][j] == 'x':
+					temp_map[i][j] = "[color=red]" + temp_map[i][j] + "[/color]"
 			rows.append(PoolStringArray(temp_map[i]).join(""))
 		var flattened_map = PoolStringArray(rows).join("\n")
 		
