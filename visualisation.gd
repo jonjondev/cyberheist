f0f268e799734fec2ecdd0364cb58df406ebf02b
Jonathan Moallem <jdmoallem@gmail.com>
2020-02-03 19:25:22 +1100

    Cleaned up visualisation script code

--- visualisation.gd
+++ visualisation.gd
@@ -1,61 +1,15 @@
 extends Node2D
 
-onready var walls = [
-	preload("res://res/wall_near.png"),
-	preload("res://res/wall_middle.png"),
-	preload("res://res/wall_far.png"),
-	preload("res://res/wall_very_far.png")
-]
-
-onready var lefts = [
-	preload("res://res/left_near.png"),
-	preload("res://res/left_middle.png"),
-	preload("res://res/left_far.png"),
-]
-
-onready var rights = [
-	preload("res://res/right_near.png"),
-	preload("res://res/right_middle.png"),
-	preload("res://res/right_far.png"),
-]
-
 func _physics_process(delta):
 	var player_location = $"../Grid".player_loc
 	var player_direction = $"../Grid".player_direction
 	var secrets = $"../Grid".secrets
 	var map = $"../Grid".wall_map
 	
-	if false:
-		$Wall.visible = false
-		$Left.visible = false
-		$Right.visible = false
-		for i in range(walls.size()):
-			if get_tile(map, player_location, player_direction, i + 1, 0) == "■":
-				$Wall.texture = walls[i]
-				$Wall.visible = true
-				for j in range(i, -1, -1):
-					if get_tile(map, player_location, player_direction, j, 1) == "■":
-						$Left.texture = lefts[j]
-						$Left.visible = true
-						break
-				for j in range(i, -1, -1):
-					if get_tile(map, player_location, player_direction, j, -1) == "■":
-						$Right.texture = rights[j]
-						$Right.visible = true
-						break
-				break
-		var alert = $"../Minimap/Alert"
-		alert.visible = false
-		for location in secrets.keys():
-			if location == player_location:
-				alert.text = "memory found:\n" + secrets[location]
-				alert.visible = true
-		$"../Minimap/TTD".text = "disconnect in: " + str($"../../Console/RichTextLabel".ttd)
-	
-	for child in get_children():
-		child.visible = false
-	
 	if map:
+		for child in get_children():
+			child.visible = false
+		
 		for i in range(6):
 			var l_tile = get_tile(map, player_location, player_direction, i, 1)
 			var f_tile = get_tile(map, player_location, player_direction, i, 0)
